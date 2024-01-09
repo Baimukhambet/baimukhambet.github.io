79eef4169ffdab4d41314b2d9ed9b997970d1de2
Timka <vendetta10090@gmail.com>
2024-01-10 04:45:43 +0600

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
+			base: process.env.NODE_ENV === 'production' ? '/baimukhambet-portfolio' : ''
 		}
 	}
 };
