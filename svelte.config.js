cb905793b7619bb7f7390fec756cb2d5d0f25c58
Timka <vendetta10090@gmail.com>
2024-01-10 06:55:25 +0600

    final

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 // const base = '/slick-portfolio-svelte';
-const base = '/baimukhambet-portfolio';
+const base = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,7 +18,7 @@ const config = {
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/baimukhambet-portfolio' : ''
+			base: process.env.NODE_ENV === 'production' ? base : ''
 		}
 	}
 };
